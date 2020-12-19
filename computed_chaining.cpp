e925750d54bd2fb34ed5a0dfb8193dd26bf24a96
dogao <doga.ozsoyler@gmail.com>
2020-12-19 16:12:34 +0300

    Add h1 and h2 functions

--- computed_chaining.cpp
+++ computed_chaining.cpp
@@ -5,10 +5,21 @@ using namespace std;
 
 int ComputedChaining::h1(int key) {
 
+    return key % data.size();
 }
 
 int ComputedChaining::h2(int key) {
 
+    int inc = (key / data.size()) % data.size();
+    
+    if(inc == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return inc;
+    }
 }
 
 
