d76314d556ddc86c2ed92cebd70513ded7a7ba0b
dogao <doga.ozsoyler@gmail.com>
2020-12-20 00:46:56 +0300

    add insert function (nat finish)

--- computed_chaining.cpp
+++ computed_chaining.cpp
@@ -25,7 +25,65 @@ int ComputedChaining::h2(int key) {
 
 void ComputedChaining::insert(int key) {
 
+    int mod = h1(key);
 
+    if(data[mod].valid == false)
+    {
+        data[mod].key = key;
+        data[mod].valid = true;
+        data[mod].plink = -1;
+    }
+    else if(data[mod].valid == true)
+    {
+        if(h1(data[mod].key) == h1(key))
+        {
+            int address = mod;
+            while(data[address].plink != -1)
+            {
+                address = h1(address + h2(data[address].key) * data[address].plink);
+            }
+
+            int newAddress = address;
+            int inc = h2(data[address].key);
+            int plink = 0;
+            int noLoop = 0;
+            while(data[newAddress].valid != false && noLoop <= data.size())
+            {
+                newAddress = h1(newAddress + inc);
+                plink++;
+                noLoop++;
+            }
+
+            if(noLoop > data.size())
+            {
+                cout << "List is full!!!" << endl;
+            }
+            else
+            {
+                data[address].plink = plink;
+                data[newAddress].key = key;
+                data[newAddress].valid = true;
+            }
+        }
+        else
+        {
+            int address = mod;
+            int oldAddress = address;
+            vector<int> removeData;
+            while(data[address].plink != -1)
+            {
+                removeData.push_back(data[address].key);
+                data[address].valid = false;
+                address = h1(address + h2(data[address].key) * data[address].plink);
+                data[oldAddress].plink = -1;
+                oldAddress = address;
+            }
+
+            while(removeData )
+
+        }
+        
+    }
 
 }
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-
+    ComputedChaining list(11);
 
 
 
